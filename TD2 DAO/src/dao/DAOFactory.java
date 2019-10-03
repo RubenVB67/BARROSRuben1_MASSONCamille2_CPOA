79d6cb149ae1ca7aa64e8f01d043d74ee2a501d7
MASSONCamille <masson.camille@ymail.com>
2019-10-03 21:42:56 +0200

    implementFactory

---
+++ TD2 DAO/src/dao/DAOFactory.java
@@ -0,0 +1,26 @@
+package dao;
+
+public class DAOFactory {
+	
+	public static DAOFactory getDAOFactory(Persistance cible) {
+		DAOFactoriy daoF = null;
+		
+		switch (cible) {
+		case MYSQL:
+		daoF = new MySQLDAOFactory();
+		break;
+		case LISTE_MEMOIRE:
+		daoF = new ListeMemoireDAOFactory();
+		break;
+		}
+
+		return doaF;
+		
+	}
+	
+	public abstract IClientDAO getIClientDAO();
+	public abstract IRevueDAO getIRevueDAO();
+	public abstract IAbonnementDAO getIAbonnementDAO();
+	public abstract IPeriodiciteDAO getIPeriodiciteDAO();
+	
+}
