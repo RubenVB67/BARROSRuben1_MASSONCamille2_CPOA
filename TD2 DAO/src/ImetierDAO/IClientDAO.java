7f9932b3a1f714c0e7628a5658b1f71e176578ae
MASSONCamille <masson.camille@ymail.com>
2019-10-03 18:59:23 +0200

    ...

--- TD2 DAO/src/ImetierDAO/IClientDAO.java
+++ TD2 DAO/src/ImetierDAO/IClientDAO.java
@@ -1,5 +1,16 @@
 package ImetierDAO;
 
-public interface IClientDAO {
+import java.util.ArrayList;
+
+import dao.IDAO;
+import metiers.Client;
+
+public interface IClientDAO extends IDAO<Client>{
+	
+	public abstract ArrayList <Client>getByNomPrenom(String nom,String prenom);
+	public abstract ArrayList <Client>getByAdresse(int no_rue,String voie);
+	public abstract ArrayList <Client>getByCodePostal(int codepostal);
+	public abstract ArrayList <Client>getByVille(String ville);
+	public abstract ArrayList <Client>getByPays(String Pays);
 
 }
