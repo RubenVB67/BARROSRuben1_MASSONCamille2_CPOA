637e37b1b7314e7942aba64a1e613ea85de67d98
Camille Masson <masson236u@iutm-inf-f36-21.iutmetz.site.univ-lorraine.fr>
2019-10-18 17:44:36 +0200

    rectif

---
+++ TD3-test/src/testlistememoire/TestListePeriodicite.java
@@ -0,0 +1,66 @@
+package testlistememoire;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import daofactory.DAOFactory;
+import daofactory.Persistance;
+import daoobjects.PeriodiciteIDAO;
+import metiers.Periodicite;
+
+class TestListePeriodicite {
+
+	PeriodiciteIDAO lper = DAOFactory.getDAOFactory(Persistance.Liste).getPeriodiciteDAO();
+	
+	@Test
+	void testCreate() {	
+		Periodicite per = new Periodicite(11, "test");
+			
+		if(!lper.create(per)) {
+			fail("Pas implemente");
+		}
+		lper.delete(per);
+	}
+
+	@Test
+	void testDelete() {	
+		Periodicite per = new Periodicite(22, "test");
+		lper.create(per);
+	
+		if(!lper.delete(per)) {
+			fail("Pas supprime");
+		}
+	}
+
+	@Test
+	void testUpdate() {	
+		Periodicite per = new Periodicite(33, "test");
+		lper.create(per);
+		
+		if(!lper.update(per)) { 
+			fail("Pas modifie");
+		}
+		lper.delete(per);
+	}
+
+	@Test
+	void testGetById() {	
+		Periodicite per = new Periodicite(44, "test");
+		lper.create(per);
+		
+		if(!lper.getById(per.getId()).equals(per)) {
+			lper.delete(per);
+			fail("Pas trouve");			
+		}
+		lper.delete(per);
+	}
+
+	@Test
+	void testFindAll() {		
+		if(lper.findAll()==null)
+		fail("Pas trouve");
+		
+	}
+
+}
---
+++ TD3-test/src/testlistememoire/TestListeRevue.java
@@ -0,0 +1,79 @@
+package testlistememoire;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import daofactory.DAOFactory;
+import daofactory.Persistance;
+import daoobjects.RevueIDAO;
+import metiers.Revue;
+
+class TestListeRevue {
+	
+	RevueIDAO revu = DAOFactory.getDAOFactory(Persistance.Liste).getRevueDAO();
+
+	@Test
+	void testCreate() {
+		 
+		Revue rev = new Revue(30, "Mangactu", "pour les fans de manga", 3., "mangactu.jpg",2);
+		
+		if(!revu.create(rev)) {
+			fail("Pas encore implemente");
+		}
+		revu.delete(rev);
+	}
+
+	@Test
+	void testDelete() {
+		 
+		Revue rev = new Revue(30, "Mangactu", "pour les fans de manga", 3., "mangactu.jpg",2);
+		
+		revu.create(rev);
+		if(!revu.delete(rev)) {
+			fail("Pas encore supprime");
+		}
+	}
+
+	@Test
+	void testUpdate() {
+		
+		Revue rev = new Revue(30, "Mangactu", "pour les fans de manga", 3., "mangactu.jpg",2);
+
+		revu.create(rev);
+		
+		if(!revu.update(rev)) {
+			fail("Pas encore modifie");
+		}
+		revu.delete(rev);
+	}
+
+	@Test
+	void testGetById() {
+		
+		Revue rev = new Revue(30, "Mangactu", "pour les fans de manga", 3., "mangactu.jpg",2);
+
+		revu.create(rev);
+		
+		if(!revu.getById(rev.getId_revue()).equals(rev)) {
+			revu.delete(rev);
+			fail("Pas trouve");			
+		}
+		revu.delete(rev);
+	}
+
+	@Test
+	void testFindAll() {	
+		
+		Revue rev = new Revue(30, "Mangactu", "pour les fans de manga", 3., "mangactu.jpg",2);
+
+		revu.create(rev);
+		
+		if(revu.findAll()==null) {
+			revu.delete(rev);
+			fail("Pas trouve");
+		}
+		revu.delete(rev);
+	}
+
+}
