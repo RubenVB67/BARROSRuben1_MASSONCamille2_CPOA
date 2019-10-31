5fdcaf20b40a1576b9a62943687feaadef6286ab
RubenVB67 <55309628+RubenVB67@users.noreply.github.com>
2019-10-31 16:20:27 +0100

    ajout controleur accueil

---
+++ TD5/src/controleur/Main.java
@@ -0,0 +1,54 @@
+package controleur;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class Main {
+
+	    @FXML
+	    private RadioButton rdb_mysql;
+
+	    @FXML
+	    private RadioButton rdb_liste;
+
+	    @FXML
+	    private Button btn_abo;
+
+	    @FXML
+	    private Button btn_client;
+
+	    @FXML
+	    private Button btn_periodicite;
+
+	    @FXML
+	    private Button btn_revue;
+
+	    @FXML
+	    void redirigerAbonnement(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void redirigerClient(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void redirigerPeriodicite(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void redirigerRevue(ActionEvent event) {
+
+	    }
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
