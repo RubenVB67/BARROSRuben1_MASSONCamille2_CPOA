5549d6eaa0b93fc534f8664c57735a52267352b9
RubenVB67 <55309628+RubenVB67@users.noreply.github.com>
2019-11-04 07:35:22 +0100

    chgmt revue perio

--- TD5/src/controleur/VuePeriodiciteControleur.java
+++ TD5/src/controleur/VuePeriodiciteControleur.java
@@ -1,11 +1,19 @@
 package controleur;
 
+import java.net.URL;
+
+import daofactory.DAOFactory;
+import daoobjects.PeriodiciteIDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import metiers.Periodicite;
 
 public class VuePeriodiciteControleur {
@@ -17,36 +25,81 @@ public class VuePeriodiciteControleur {
     private TableView<Periodicite> tbl_periodicite;
 
     @FXML
-    private Pane pnl_top;
+    private TableColumn<Periodicite, Integer> col_perioidperio;
 
     @FXML
-    private RadioButton rdb_sql;
+    private TableColumn<Periodicite, String> col_libelle;
 
     @FXML
-    private RadioButton rdb_liste;
+    private Button btn_supprimer;
 
     @FXML
-    private Button btn_client;
+    private Button btn_modifier;
 
     @FXML
-    private Button btn_abonnement;
+    private Button btn_creer;
 
     @FXML
-    private Button btn_revue;
+    private Button btn_retour;
+    
+    public boolean creer = false;
+    private DAOFactory daof;
+    private PeriodiciteIDAO perio; 
 
     @FXML
-    void redirigerAbonnement(ActionEvent event) {
-
+    void creerPeriodicite(ActionEvent event) {
+    	creer = true;
+    	try {
+			URL fxmlURL = getClass().getResource("/fenetre/DetailPeriodicite.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Parent root = fxmlLoader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Création d'une periodicité");
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
-    void redirigerClient(ActionEvent event) {
-
+    void modifierPeriodicite(ActionEvent event) {
+    	creer = false;
+    	try {
+			URL fxmlURL = getClass().getResource("/fenetre/DetailPeriodicite.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Parent root = fxmlLoader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Modification d'une periodicité");
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
-    void redirigerRevue(ActionEvent event) {
+    void retourAccueil(ActionEvent event) {
+    	Stage stage = (Stage) btn_retour.getScene().getWindow();
+        stage.close();
+    }
 
+    @FXML
+    void supprimerPeriodicite(ActionEvent event) {
+    	try {
+			URL fxmlURL = getClass().getResource("/fenetre/Supprimer.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Parent root = fxmlLoader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Suppression d'une periodicité");
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
 }
--- TD5/src/controleur/VueRevueControleur.java
+++ TD5/src/controleur/VueRevueControleur.java
@@ -1,45 +1,142 @@
 package controleur;
 
+import java.net.URL;
+import java.util.List;
+
+import daofactory.Connexion;
+import daofactory.DAOFactory;
+import daofactory.Persistance;
+import daoobjects.RevueIDAO;
 import javafx.event.ActionEvent;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import metiers.Periodicite;
 import metiers.Revue;
 
 public class VueRevueControleur {
 
     @FXML
     private TableView<Revue> tbl_revue;
+    
+    @FXML
+    private TableColumn<Revue,Integer> col_revidrev;
 
     @FXML
-    private Button btn_abonnement;
+    private TableColumn<Revue, Integer> col_revidperio;
 
     @FXML
-    private Button btn_client;
+    private TableColumn<Revue, String> col_titre;
 
     @FXML
-    private Button btn_periodicite;
+    private TableColumn<Revue, String> col_description;
 
     @FXML
-    private RadioButton rdb_sql;
+    private TableColumn<Revue, String> col_visuel;
 
     @FXML
-    private RadioButton rdb_liste;
+    private TableColumn<Revue, Double> col_tarif;
 
     @FXML
-    void redirigerAbonnement(ActionEvent event) {
+    private Button btn_creer;
 
+    @FXML
+    private Button btn_modifier;
+
+    @FXML
+    private Button btn_supprimer;
+
+    @FXML
+    private Button btn_retour;
+    
+    @FXML
+    private Label lbl_perio;
+    
+    @FXML
+    private Label lbl_aucunchamp;
+    
+    public boolean creer = false;
+    private DAOFactory daof;
+    private RevueIDAO rev;
+    
+    public void initialize() {
+		this.daof = DAOFactory.getDAOFactory(Persistance.Liste);
+		rev = this.daof.getRevueDAO();
+        col_revidrev.setCellValueFactory(new PropertyValueFactory<>("Id_revue"));
+        col_revidperio.setCellValueFactory(new PropertyValueFactory<>("Id_periodicite"));
+        col_titre.setCellValueFactory(new PropertyValueFactory<>("Titre"));
+        col_description.setCellValueFactory(new PropertyValueFactory<>("Description"));
+        col_visuel.setCellValueFactory(new PropertyValueFactory<>("Visuel"));
+        col_tarif.setCellValueFactory(new PropertyValueFactory<>("Tarif_numero"));
+		tbl_revue.setItems(FXCollections.observableArrayList(rev.findAll()));
     }
 
     @FXML
-    void redirigerClient(ActionEvent event) {
-
+    void creerRevue(ActionEvent event) {
+    	creer = true;
+    	try {
+			URL fxmlURL = getClass().getResource("/fenetre/DetailRevue.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Parent root = fxmlLoader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Création d'une revue");
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
-    void redirigerPeriodicite(ActionEvent event) {
-
+    void modifierRevue(ActionEvent event) {
+    	creer = false;
+    		try {
+    			URL fxmlURL = getClass().getResource("/fenetre/DetailRevue.fxml");
+    			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+    			Parent root = fxmlLoader.load();
+    			Stage stage = new Stage();
+    			Scene scene = new Scene(root);
+    			stage.setScene(scene);
+    			stage.setTitle("Modification d'une revue");
+    			stage.show();
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
     }
 
+    @FXML
+    void retourAccueil(ActionEvent event) {
+    	Stage stage = (Stage) btn_retour.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void supprimerRevue(ActionEvent event) {
+    	try {
+			URL fxmlURL = getClass().getResource("/fenetre/Supprimer.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Parent root = fxmlLoader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Suppression d'une revue");
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
